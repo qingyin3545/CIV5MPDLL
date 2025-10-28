b540d40c9b39cfd4b342f0d2690ae7eaa13c2220
touchfishking <3138584199@qq.com>
2025-10-28 14:36:04 +0800

    format: Technology DB

--- (1) CIV5MPDLL/DB/TECHS/DDL_Technologies.sql
+++ (1) CIV5MPDLL/DB/TECHS/DDL_Technologies.sql
@@ -1,4 +1,8 @@
-alter table Technologies add column RazeSpeedModifier integer not null default 0;
-alter table Technologies add column FreePromotionRemoved integer default -1;
-alter table Technologies add column RemoveCurrentPromotion boolean default 0;
-alter table Technologies add column RemoveOceanImpassableCivilian boolean default 0;
+alter table Technologies add RazeSpeedModifier integer not null default 0;
+alter table Technologies add FreePromotionRemoved integer default -1;
+alter table Technologies add RemoveCurrentPromotion boolean default 0;
+alter table Technologies add RemoveOceanImpassableCivilian boolean default 0;
+
+alter table Technologies add CitySplashDamage boolean default 0;
+alter table Technologies add BombardRange integer default 0;
+alter table Technologies add BombardIndirect integer default 0;
